899b9d3fc38a20a91df9bd217a17cda8f4f062c2
inseong01 <insung940@naver.com>
2025-11-11 14:44:18 +0900

    feat: Application 구현
    - RacingController 메서드 배치
    - 예제 테스트 통과

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(String[] args) {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    RaceController raceController = new RaceController(inputView, outputView);
+
+    raceController.setCarNames();
+
+    raceController.setRepeatNumber();
+
+    raceController.playGame();
+
+    raceController.resultRace();
+  }
 }
